842943dae3d8593ebe72a0814367642d9637e0f1
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-07-01 11:18:59 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,19 @@
 const profile = {
-    newName: "Jacob",
+    username: "Jacob",
     playTime: 300,
-
+    
     changeUsername(newName) {
-        this.userName = newName;
+        this.username = newName;
     },
-
+    
     updatePlayTime(hours) {
         this.playTime += hours;
-    }
-
+    },
+    
     getInfo() {
         return `${this.username} has ${this.playTime} active hours!`;
     }
 };
-
-
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
@@ -24,4 +22,3 @@ console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
-
